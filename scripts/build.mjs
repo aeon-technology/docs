6d0065c6051bc9e48e3899f88e2ea5d46c03fbd0
Chuanqi Sun <chuanqisun-no-reply@gmail.com>
2022-07-02 09:46:43 -0700

    single file output

--- scripts/build.mjs
+++ scripts/build.mjs
@@ -1,9 +1,9 @@
 import esbuild from "esbuild";
 import fse from "fs-extra";
-import { mkdir, readdir, readFile, writeFile } from "fs/promises";
+import fs from "fs/promises";
 import { shiftHeading } from "hast-util-shift-heading";
 import yaml from "js-yaml";
-import { dirname, extname, join, relative, resolve } from "path";
+import path from "path";
 import rehypeAutolinkHeadings from "rehype-autolink-headings";
 import rehypeSlug from "rehype-slug";
 import rehypeStringify from "rehype-stringify";
@@ -14,16 +14,16 @@ import remarkRehype from "remark-rehype";
 import { unified } from "unified";
 import { fileURLToPath } from "url";
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const isDevelopement = process.argv.includes("--dev");
 
 const DIR = {
-  src: resolve(__dirname, "../src"),
-  dist: resolve(__dirname, "../dist"),
-  content: resolve(__dirname, "../content"),
-  public: resolve(__dirname, "../public"),
-  media: resolve(__dirname, "../content/media"),
+  src: path.resolve(__dirname, "../src"),
+  dist: path.resolve(__dirname, "../dist"),
+  content: path.resolve(__dirname, "../content"),
+  public: path.resolve(__dirname, "../public"),
+  media: path.resolve(__dirname, "../content/media"),
 };
 
 const buildMd = unified()
@@ -62,42 +62,52 @@ async function main() {
   const css = await buildCss();
   await copyAssets();
 
-  const indexTemplate = await readFile(join(DIR.src, "index.html"), "utf-8");
-  const indexHtml = indexTemplate
-    .replace(
-      "{{ pages }}",
-      mdPages
-        .sort((a, b) => a.meta.path.localeCompare(b.meta.path))
-        .map(renderPageHtml)
-        .join("\n")
-    )
-    .replace("{{ beforeHeadEnd }}", `<style>${css.outputFiles[0].text}</style>`)
-    .replace(
-      "{{ beforeBodyEnd }}",
-      `<script type="module">${js.outputFiles[0].text}</script>`
-    );
+  const indexTemplate = await fs.readFile(
+    path.join(DIR.src, "index.html"),
+    "utf-8"
+  );
 
-  const indexOutPath = join(DIR.dist, "index.html");
-  await ensureDir(indexOutPath);
-  await writeFile(indexOutPath, indexHtml);
+  const tasks = mdPages
+    .map((mdPage) => ({
+      html: indexTemplate
+        .replace("{{ main }}", renderPageHtml(mdPage))
+        .replace(
+          "{{ beforeHeadEnd }}",
+          `<style>${css.outputFiles[0].text}</style>`
+        )
+        .replace(
+          "{{ beforeBodyEnd }}",
+          `<script type="module">${js.outputFiles[0].text}</script>`
+        ),
+      outPath: path.join(
+        "dist",
+        replaceExtension(mdPage.meta.srcRelativePath, ".html")
+      ),
+    }))
+    .map(async (task) => {
+      await ensureDir(task.outPath);
+      await fs.writeFile(task.outPath, task.html);
+    });
+
+  await Promise.all(tasks);
 }
 
 async function buildMdPages() {
   const markdownPaths = (await getFiles(DIR.content)).filter((filePath) =>
-    [".md"].includes(extname(filePath))
+    [".md"].includes(path.extname(filePath))
   );
 
   const pages = await Promise.all(
     markdownPaths.map(async (markdownPath) => {
-      const mdString = await readFile(markdownPath, "utf-8");
+      const mdString = await fs.readFile(markdownPath, "utf-8");
       const htmlFile = await buildMd.process(mdString);
-      const path = relative(DIR.content, markdownPath);
+      const outPath = path.relative(DIR.content, markdownPath);
 
       const page = {
         html: String(htmlFile),
         meta: {
           ...htmlFile.meta,
-          path,
+          srcRelativePath: outPath,
         },
       };
 
@@ -135,23 +145,31 @@ async function buildJs() {
 }
 
 async function copyAssets() {
-  await fse.copy(DIR.public, join(DIR.dist));
-  await fse.copy(DIR.media, join(DIR.dist, "media"));
+  await fse.copy(DIR.public, path.join(DIR.dist));
+  await fse.copy(DIR.media, path.join(DIR.dist, "media"));
 }
 
 function renderPageHtml(page) {
   return `<article>${page.html}</article>`;
 }
 
+function replaceExtension(srcPath, toExt) {
+  return path.format({
+    ...path.parse(srcPath),
+    base: "",
+    ext: toExt,
+  });
+}
+
 async function ensureDir(dir) {
-  return mkdir(dirname(dir), { recursive: true });
+  return fs.mkdir(path.dirname(dir), { recursive: true });
 }
 
 async function getFiles(dir) {
-  const dirents = await readdir(dir, { withFileTypes: true });
+  const dirents = await fs.readdir(dir, { withFileTypes: true });
   const files = await Promise.all(
     dirents.map((dirent) => {
-      const res = resolve(dir, dirent.name);
+      const res = path.resolve(dir, dirent.name);
       return dirent.isDirectory() ? getFiles(res) : res;
     })
   );
