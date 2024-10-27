8cb1243edeb330a54db8d95632379fe3064050a9
Chuanqi Sun <chuanqisun-no-reply@gmail.com>
2024-10-27 09:52:32 -0700

    add fragment converter

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 console.log("ui ready");
 
-[...document.querySelectorAll("nav a")]
-  .find((a) => a.pathname === location.pathname)
-  ?.classList.add("active");
+[...document.querySelectorAll("nav a")].find((a) => a.pathname === location.pathname)?.classList.add("active");
+
+// Convert `?` search parameter to `#` hash
+const fragment = new URLSearchParams(location.search).get("fragment");
+if (fragment) {
+  location.hash = fragment;
+}
