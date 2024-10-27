180c27ca28ed56b12b08f1c83590d35a76ee3d44
Chuanqi Sun <chuanqisun-no-reply@gmail.com>
2024-10-27 10:48:52 -0700

    add nav header

--- src/index.js
+++ src/index.js
@@ -1,9 +1,29 @@
-console.log("ui ready");
+highlightActiveNavItem();
+handleUrlFragmentFromSearchParameter();
+enableBackForwardNavButton();
 
-[...document.querySelectorAll("nav a")].find((a) => a.pathname === location.pathname)?.classList.add("active");
+
+function highlightActiveNavItem() {
+  [...document.querySelectorAll("nav a")].find((a) => a.pathname === location.pathname)?.classList.add("active");
+}
 
 // Convert `?` search parameter to `#` hash
-const fragment = new URLSearchParams(location.search).get("fragment");
-if (fragment) {
-  location.hash = fragment;
+function handleUrlFragmentFromSearchParameter() {
+  const fragment = new URLSearchParams(location.search).get("fragment");
+  if (fragment) {
+    location.hash = fragment;
+  }
+}
+
+function enableBackForwardNavButton() {
+  document.querySelector("#nav-buttons").removeAttribute("hidden");
+  document.querySelector("#go-next").onclick = () => navigateByOffset(1);
+  document.querySelector("#go-prev").onclick = () => navigateByOffset(-1);
+}
+
+function navigateByOffset(offset) {
+  const flattenedLinks = [...document.querySelectorAll("nav a")];
+  const activeElement = document.querySelector("nav a.active");
+  const nextElement = flattenedLinks[flattenedLinks.indexOf(activeElement) + offset];
+  nextElement?.click();
 }
